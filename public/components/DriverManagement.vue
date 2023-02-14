4d90aac75c3b5f6978cae3a1711e2c426d5fbb0b
Duc Hoang <hoang@stocard.de>
2023-02-14 13:11:09 +0100

    feat: implement main page components

---
+++ public/components/DriverManagement.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <button class="button" @click="addButtonClicked()">Add driver</button>
+
+    <div style="height: 20px"></div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="driver in drivers" :key="driver.id">
+          <td>{{ driver.name }}</td>
+          <td>{{ driver.location }}</td>
+          <td>
+            <button class="button" @click="updateButtonClicked(driver.id)">
+              Update
+            </button>
+            <button class="button" @click="deleteButtonClicked(driver.id)">
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <AddDriverModalVue
+      v-if="showAddDriverModal"
+      @modalClosed="showAddDriverModal = false"
+      @newDriverAdded="getDrivers"
+    ></AddDriverModalVue>
+    <UpdateDriverModalVue
+      v-if="showUpdateDriverModal"
+      :driver-id="driverIdInProcess"
+      @modalClosed="
+        showUpdateDriverModal = false
+        resetDriverIdInProcess()
+      "
+      @driverUpdated="getDrivers"
+    ></UpdateDriverModalVue>
+    <DeleteDriverModalVue
+      v-if="showDeleteDriverModal"
+      :driver-id="driverIdInProcess"
+      @modalClosed="
+        showDeleteDriverModal = false
+        resetDriverIdInProcess()
+      "
+      @driverDeleted="getDrivers"
+    ></DeleteDriverModalVue>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+import { ServerRoute } from '@enum/ServerRoute'
+
+import AddDriverModalVue from './modal/AddDriverModal.vue'
+import UpdateDriverModalVue from './modal/UpdateDriverModal.vue'
+import DeleteDriverModalVue from './modal/DeleteDriverModal.vue'
+
+type ComponentData = {
+  drivers: {
+    id: string
+    name: string
+    location: string
+  }[]
+  showAddDriverModal: boolean
+  showUpdateDriverModal: boolean
+  showDeleteDriverModal: boolean
+}
+
+export default {
+  components: {
+    AddDriverModalVue,
+    UpdateDriverModalVue,
+    DeleteDriverModalVue,
+  },
+  data(): ComponentData {
+    return {
+      drivers: [],
+      showAddDriverModal: false,
+      showUpdateDriverModal: false,
+      showDeleteDriverModal: false,
+    }
+  },
+  async created(): Promise<void> {
+    await this.getDrivers()
+  },
+  methods: {
+    async getDrivers(): Promise<void> {
+      try {
+        const uri = ServerRoute.Driver
+        const response = await axios.get(uri)
+        const { drivers } = response.data
+        this.drivers = drivers
+        return
+      } catch (error) {
+        alert('Something went wrong. Please try again.')
+        console.log(error)
+      }
+    },
+    addButtonClicked(): void {
+      this.showAddDriverModal = true
+    },
+    updateButtonClicked(driverId: string): void {
+      this.driverIdInProcess = driverId
+      this.showUpdateDriverModal = true
+    },
+    deleteButtonClicked(driverId: string): void {
+      this.driverIdInProcess = driverId
+      this.showDeleteDriverModal = true
+    },
+    resetDriverIdInProcess(): void {
+      this.driverIdInProcess = ''
+    }
+  },
+}
+</script>
---
+++ public/components/TourManagement.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <button class="button" @click="addButtonClicked()">Add tour</button>
+
+    <div style="height: 20px"></div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Customer name</th>
+          <th>Shipment date</th>
+          <th>Location from</th>
+          <th>Location to</th>
+          <th>Assigned driver</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="tour in tours" :key="tour.id">
+          <td>{{ tour.customerName }}</td>
+          <td>{{ tour.shipmentDate }}</td>
+          <td>{{ tour.locationFrom }}</td>
+          <td>{{ tour.locationTo }}</td>
+          <td>{{ tour.assignedDriver }}</td>
+          <td style="width: min-content;">
+            <button class="button" @click="updateButtonClicked(tour.id)">Update</button>
+            <button class="button" @click="deleteButtonClicked(tour.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <AddTourModalVue
+      v-if="showAddTourModal"
+      @modalClosed="showAddTourModal = false"
+      @newTourAdded="getTours"
+    ></AddTourModalVue>
+    <UpdateTourModalVue
+      v-if="showUpdateTourModal"
+      :tour-id="tourIdInProcess"
+      @modalClosed="showUpdateTourModal = false; resetTourIdInProcess();"
+      @tourUpdated="getTours"
+    ></UpdateTourModalVue>
+    <DeleteTourModalVue
+      v-if="showDeleteTourModal"
+      :tour-id="tourIdInProcess"
+      @modalClosed="showDeleteTourModal = false; resetTourIdInProcess();"
+      @tourDeleted="getTours"
+    ></DeleteTourModalVue>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+
+import { ServerRoute } from '@enum/ServerRoute'
+
+import AddTourModalVue from './modal/AddTourModal.vue'
+import UpdateTourModalVue from './modal/UpdateTourModal.vue'
+import DeleteTourModalVue from './modal/DeleteTourModal.vue'
+
+type ComponentData = {
+  tours: {
+    id: string
+    customerName: string
+    shipmentDate: string
+    locationFrom: string
+    locationTo: string
+    assignedDriver: string
+  }[]
+  tourIdInProcess: string
+  showAddTourModal: boolean
+  showUpdateTourModal: boolean
+  showDeleteTourModal: boolean
+}
+
+export default {
+  components: {
+    AddTourModalVue,
+    UpdateTourModalVue,
+    DeleteTourModalVue,
+  },
+  data(): ComponentData {
+    return {
+      tours: [],
+      tourIdInProcess: '',
+      showAddTourModal: false,
+      showUpdateTourModal: false,
+      showDeleteTourModal: false,
+    }
+  },
+  async created(): Promise<void> {
+    await this.getTours()
+  },
+  methods: {
+    async getTours(): Promise<void> {
+      try {
+        const uri = ServerRoute.Tour
+        const response = await axios.get(uri)
+        const { tours } = response.data
+        this.tours = tours
+        return
+      } catch (error) {
+        alert('Something went wrong. Please try again.')
+        console.log(error)
+      }
+    },
+    addButtonClicked(): void {
+      this.showAddTourModal = true
+    },
+    updateButtonClicked(tourId: string): void {
+      this.tourIdInProcess = tourId
+      this.showUpdateTourModal = true
+    },
+    deleteButtonClicked(tourId: string): void {
+      this.tourIdInProcess = tourId
+      this.showDeleteTourModal = true
+    },
+    resetTourIdInProcess(): void {
+      this.tourIdInProcess = ''
+    }
+  },
+}
+</script>
