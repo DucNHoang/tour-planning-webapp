ad8618e83b5cbeff09fd165ba842782ae685956e
Duc Hoang <hoang@stocard.de>
2023-02-14 13:10:39 +0100

    feat: implement FreeTextInput component

---
+++ public/components/FreeTextInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-container">
+    <label>{{ label }}:</label>
+    <input
+      v-model="inputValue"
+      :readonly="readonly"
+      type="text"
+      @input="processInput"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+type ComponentData = {
+  inputValue: string
+}
+
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data(): ComponentData {
+    return {
+      inputValue: this.value,
+    }
+  },
+  methods: {
+    processInput() {
+      this.$emit('input', this.inputValue)
+    },
+  },
+}
+</script>
